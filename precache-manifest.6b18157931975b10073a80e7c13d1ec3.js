414c4f0ca6fdfc1771b007248b0adbd85399f980
li2he1 <peterli2he1@gmail.com>
2020-09-19 18:58:18 -0400

    Updates

---
+++ precache-manifest.6b18157931975b10073a80e7c13d1ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e018e2ff9336df51892a77dafb5d4e5",
+    "url": "/github-pages-simple-meal-LH/index.html"
+  },
+  {
+    "revision": "12dee87adca8398940d9",
+    "url": "/github-pages-simple-meal-LH/static/css/main.9dcab7d2.chunk.css"
+  },
+  {
+    "revision": "27ac2c15469fa30c15e7",
+    "url": "/github-pages-simple-meal-LH/static/js/2.65c59741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-pages-simple-meal-LH/static/js/2.65c59741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12dee87adca8398940d9",
+    "url": "/github-pages-simple-meal-LH/static/js/main.a641319f.chunk.js"
+  },
+  {
+    "revision": "e1159d71bbbb2bf2e6a9",
+    "url": "/github-pages-simple-meal-LH/static/js/runtime-main.e54d3ed9.js"
+  }
+]);
