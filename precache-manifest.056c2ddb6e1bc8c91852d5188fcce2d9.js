d89143caeb0381db93b67110cf58324af4857833
li2he1 <peterli2he1@gmail.com>
2020-09-19 19:40:49 -0400

    Updates

---
+++ precache-manifest.056c2ddb6e1bc8c91852d5188fcce2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d1a481946690939af9ab62807c3031",
+    "url": "/github-pages-simple-meal/index.html"
+  },
+  {
+    "revision": "26e9fe4c3d77b6223618",
+    "url": "/github-pages-simple-meal/static/css/main.9dcab7d2.chunk.css"
+  },
+  {
+    "revision": "27ac2c15469fa30c15e7",
+    "url": "/github-pages-simple-meal/static/js/2.65c59741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-pages-simple-meal/static/js/2.65c59741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e9fe4c3d77b6223618",
+    "url": "/github-pages-simple-meal/static/js/main.bc2ea283.chunk.js"
+  },
+  {
+    "revision": "99b422f0e29ef27853a7",
+    "url": "/github-pages-simple-meal/static/js/runtime-main.a6d7bfdc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-pages-simple-meal-LH/precache-manifest.da51e952f6699caff0db822e688993ad.js"
+  "/github-pages-simple-meal/precache-manifest.056c2ddb6e1bc8c91852d5188fcce2d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-pages-simple-meal-LH/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-pages-simple-meal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
